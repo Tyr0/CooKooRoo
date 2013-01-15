b641fa83a7f3ea648fe998e486b2316b4fd6ee94
Tyler Calderone <tyler@tylercalderone.com>
2013-01-14 17:18:23 -0800

    Initial code commit, just an empty iOS application for now

---
+++ CooKooRoo/CooKooRoo/CKRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CKRAppDelegate.h
+//  CooKooRoo
+//
+//  Created by Tyler Calderone on 1/14/13.
+//  Copyright (c) 2013 Tyler Calderone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CKRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CooKooRoo/CooKooRoo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CooKooRoo
+//
+//  Created by Tyler Calderone on 1/14/13.
+//  Copyright (c) 2013 Tyler Calderone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CKRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CKRAppDelegate class]));
+    }
+}
